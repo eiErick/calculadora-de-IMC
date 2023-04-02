b680e1731547b57fce3749405624b5acd22da27d
Érick <eierickdasilva@gmail.com>
2023-04-02 11:15:29 -0300

    initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+var range = document.getElementById("range");
+var altura = document.getElementById("altura")
+var valor = parseInt(range.value);
+var valoraltura = parseInt(altura.value);
+var alturateste = valoraltura / 100
+var teste = document.getElementById("teste")
+var valorteste = valor / 10
+
+//alert(` peso ${valorteste}KG ${alturateste}metros`)
